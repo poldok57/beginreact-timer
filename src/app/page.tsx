b1ca9728ed7841bd6fb9e6b5500165857ecd5dcd
poldok57 <rump.guy@orange.fr>
2024-06-30 18:21:03 +0200

    correction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 "use client";
-import { Timer } from "../types/timer";
-import { useTimersStore } from "../hooks/zustand/timers";
 import { TimerInput } from "./TimerInput";
 import { TimerList } from "./TimerList";
 export default function Home() {
-  const { timers, addTimer, getTimer } = useTimersStore();
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4 lg:p-12">
       <TimerInput />
